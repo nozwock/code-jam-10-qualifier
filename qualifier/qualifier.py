9226e49dae9f1a5b1a79f52dae7180e4841d01db
nozwock <57829219+nozwock@users.noreply.github.com>
2023-08-18 00:02:16 +0530

    Implement `rearrange_tiles`

--- qualifier/qualifier.py
+++ qualifier/qualifier.py
@@ -1,8 +1,11 @@
 from functools import reduce
 from typing import Any, Iterable
 
+import numpy as np
+from PIL import Image
 
-def prod(seq: Iterable[Any]) -> Any:  # meh, bounds feels like a mess
+
+def prod(seq: Iterable[Any]) -> Any:  # meh, bounding feels like a mess
     return reduce(lambda acc, i: acc * i, seq)
 
 
@@ -39,4 +42,38 @@ def rearrange_tiles(
     "The tile size or ordering are not valid for the given image".
     """
 
-    ...
+    img: Image.Image
+    with Image.open(image_path) as img:
+        if not valid_input(img.size, tile_size, ordering):
+            raise ValueError(
+                "The tile size or ordering are not valid for the given image"
+            )
+
+        # use img.mode, output file must have same mode
+
+        img_arr = np.asarray(img)
+        img_height, img_width, channels = img_arr.shape
+        tile_height, tile_width = tile_size
+
+        rows = img_height // tile_height
+        cols = img_width // tile_width
+        tiles = img_arr.reshape(
+            rows,  # row
+            tile_height,
+            cols,  # column
+            tile_width,
+            channels,
+        ).swapaxes(1, 2)
+
+        arranged_img_arr = np.zeros(tiles.shape, dtype=np.uint8)
+
+        for i, order in enumerate(ordering):
+            from_row, from_col = divmod(i, cols)
+            to_row, to_col = divmod(order, cols)
+            arranged_img_arr[from_row, from_col] = tiles[
+                to_row, to_col
+            ]  # wtf is even happening??? why from to to???
+
+        arranged_img_arr = arranged_img_arr.swapaxes(1, 2).reshape(img_arr.shape)
+        arranged_img = Image.fromarray(arranged_img_arr)
+        arranged_img.save(out_path)
